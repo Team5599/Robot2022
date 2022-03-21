7b4858d31c60b140c6c60487eaa96d7a2460d099
Matthew McCall <46231996+mxtt-mmxix@users.noreply.github.com>
2022-03-21 15:41:56 -0400

    Shooter and Intake

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -20,141 +20,163 @@ import com.revrobotics.CANSparkMax;
 import frc.robot.Controllers.XBoxController;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the
+ * build.gradle file in the
  * project.
  */
 
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
-  
-  // Declaration of Objects
-  // Falcon FX (Falcon 500) Motors
-  WPI_TalonFX l0, l1, r0, r1;
-  CANSparkMax intake, shooterPivot, s0, s1;
+    private static final String kDefaultAuto = "Default";
+    private static final String kCustomAuto = "My Auto";
+    private String m_autoSelected;
+    private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  MotorControllerGroup lDrive, rDrive, shooter;
+    // Declaration of Objects
+    // Falcon FX (Falcon 500) Motors
+    WPI_TalonFX l0, l1, r0, r1;
+    CANSparkMax intake, shooterPivot, s0, s1;
 
-  // Drivetrain
-  DifferentialDrive drivetrain;
+    MotorControllerGroup lDrive, rDrive, shooter;
 
-  // Controller
-  XBoxController ctrl;
+    // Drivetrain
+    DifferentialDrive drivetrain;
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
-  @Override
-  public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
+    // Controller
+    XBoxController ctrl;
 
-    // Initialize objects
-    ctrl = new XBoxController(0);
+    /**
+     * This function is run when the robot is first started up and should be used
+     * for any
+     * initialization code.
+     */
+    @Override
+    public void robotInit() {
+        m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+        m_chooser.addOption("My Auto", kCustomAuto);
+        SmartDashboard.putData("Auto choices", m_chooser);
 
-    // Left Falcon motor(s)
-    l0 = new WPI_TalonFX(0);
-    l1 = new WPI_TalonFX(1); 
+        // Initialize objects
+        ctrl = new XBoxController(0);
 
-    // right Falcon motor(s)
-    r0 = new WPI_TalonFX(3);
-    r1 = new WPI_TalonFX(4); 
-    
-    s0 = new CANSparkMax(8, MotorType.kBrushless);
-    s1 = new CANSparkMax(9, MotorType.kBrushless);
-    
-    intake = new CANSparkMax(6, MotorType.kBrushless);
-    shooterPivot = new CANSparkMax(7, MotorType.kBrushless);
+        // Left Falcon motor(s)
+        l0 = new WPI_TalonFX(0);
+        l1 = new WPI_TalonFX(1);
 
-    l0.configFactoryDefault();
-    l1.configFactoryDefault();
+        // right Falcon motor(s)
+        r0 = new WPI_TalonFX(3);
+        r1 = new WPI_TalonFX(4);
 
-    r0.configFactoryDefault();
-    r1.configFactoryDefault();
+        s0 = new CANSparkMax(8, MotorType.kBrushless);
+        s1 = new CANSparkMax(9, MotorType.kBrushless);
 
-    shooter = new MotorControllerGroup(s0, s1);
+        intake = new CANSparkMax(6, MotorType.kBrushless);
+        shooterPivot = new CANSparkMax(7, MotorType.kBrushless);
 
-    // left drivetrain
-    lDrive = new MotorControllerGroup(l0, l1);
+        l0.configFactoryDefault();
+        l1.configFactoryDefault();
 
-    // right drivetrain
-    rDrive = new MotorControllerGroup(r0, r1);
+        r0.configFactoryDefault();
+        r1.configFactoryDefault();
 
-    // drivetrain
-    drivetrain = new DifferentialDrive(lDrive, rDrive);
-  }
+        shooter = new MotorControllerGroup(s0, s1);
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-  }
+        // left drivetrain
+        lDrive = new MotorControllerGroup(l0, l1);
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
-   */
-  @Override
-  public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
-  }
+        // right drivetrain
+        rDrive = new MotorControllerGroup(r0, r1);
 
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
+        // drivetrain
+        drivetrain = new DifferentialDrive(lDrive, rDrive);
     }
-  }
 
-  /** This function is called once when teleop is enabled. */
-  @Override
-  public void teleopInit() {}
+    /**
+     * This function is called every robot packet, no matter the mode. Use this for
+     * items like
+     * diagnostics that you want ran during disabled, autonomous, teleoperated and
+     * test.
+     *
+     * <p>
+     * This runs after the mode specific periodic functions, but before LiveWindow
+     * and
+     * SmartDashboard integrated updating.
+     */
+    @Override
+    public void robotPeriodic() {
+    }
 
-  /** This function is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {
-    drivetrain.tankDrive(ctrl.getLeftThumbstickY(), ctrl.getRightThumbstickY());
-  }
+    /**
+     * This autonomous (along with the chooser code above) shows how to select
+     * between different
+     * autonomous modes using the dashboard. The sendable chooser code works with
+     * the Java
+     * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the
+     * chooser code and
+     * uncomment the getString line to get the auto name from the text box below the
+     * Gyro
+     *
+     * <p>
+     * You can add additional auto modes by adding additional comparisons to the
+     * switch structure
+     * below with additional strings. If using the SendableChooser make sure to add
+     * them to the
+     * chooser code above as well.
+     */
+    @Override
+    public void autonomousInit() {
+        m_autoSelected = m_chooser.getSelected();
+        // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+        System.out.println("Auto selected: " + m_autoSelected);
+    }
 
-  /** This function is called once when the robot is disabled. */
-  @Override
-  public void disabledInit() {}
+    /** This function is called periodically during autonomous. */
+    @Override
+    public void autonomousPeriodic() {
+        switch (m_autoSelected) {
+            case kCustomAuto:
+                // Put custom auto code here
+                break;
+            case kDefaultAuto:
+            default:
+                // Put default auto code here
+                break;
+        }
+    }
 
-  /** This function is called periodically when disabled. */
-  @Override
-  public void disabledPeriodic() {}
+    /** This function is called once when teleop is enabled. */
+    @Override
+    public void teleopInit() {
+    }
 
-  /** This function is called once when test mode is enabled. */
-  @Override
-  public void testInit() {}
+    /** This function is called periodically during operator control. */
+    @Override
+    public void teleopPeriodic() {
+        drivetrain.tankDrive(ctrl.getLeftThumbstickY(), ctrl.getRightThumbstickY());
+        intake.set(ctrl.getLeftTriggerAbsolute());
+        shooter.set(ctrl.getRightTriggerAbsolute());
+    }
 
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {}
+    /** This function is called once when the robot is disabled. */
+    @Override
+    public void disabledInit() {
+    }
+
+    /** This function is called periodically when disabled. */
+    @Override
+    public void disabledPeriodic() {
+    }
+
+    /** This function is called once when test mode is enabled. */
+    @Override
+    public void testInit() {
+    }
+
+    /** This function is called periodically during test mode. */
+    @Override
+    public void testPeriodic() {
+    }
 }
